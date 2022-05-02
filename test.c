8c83d3aa5e8bf4b250e055255ae9eb9d940d907e
LordMefloun <realfrog42@gmail.com>
2022-05-02 16:53:41 +0200

    test

---
+++ test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+
+    //Answer the question Who is the main singer of band Bring Me The Horizon in comment bellow
+    //Answer:
+    //1. The main singer is Eric Clapton
+    
+
+
+
+
+}
