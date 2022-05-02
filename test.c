87fe09c72539f9ee0f2d5403b43e248fe1e5e4f1
LordMefloun <realfrog42@gmail.com>
2022-05-02 16:55:12 +0200

    test

--- test.c
+++ test.c
@@ -4,9 +4,8 @@ int main()
 {
 
     //Answer the question Who is the main singer of band Bring Me The Horizon in comment bellow
-    //Answer:
-    //1. The main singer is Eric Clapton
-    
+    //Answer: main singer is Kokotkovia
+
 
 
 
